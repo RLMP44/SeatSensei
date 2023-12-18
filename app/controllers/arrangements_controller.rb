49a2413e360a597b4bbd9a9147d05b295f5a174c
Michael Sam <sam.michael.h@gmail.com>
2023-12-18 16:52:11 +0900

    Add controllers

---
+++ app/controllers/arrangements_controller.rb
@@ -0,0 +1,2 @@
+class ArrangementsController < ApplicationController
+end
---
+++ test/controllers/s_classes_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SClassesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
