29a6b25c33dc951e5348bce43bc9fe38bac89062
Rina Iwamine <leddihome@gmail.com>
2023-12-18 17:03:26 +0900

    updated routes

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,6 +1,6 @@
 class Seat < ApplicationRecord
   belongs_to :student
-  belongs_to :classroom
+  belongs_to :classroom, dependent: :destroy
 
   validates :row, numericality: true
   validates :column, numericality: true
