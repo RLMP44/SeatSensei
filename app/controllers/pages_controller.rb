87d278c3de013fce1ce770ea02bb51204fd9b7b9
Rachael Momber <rlmomber@gmail.com>
2023-12-20 16:40:53 +0900

    added dashboard and edited pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @classrooms = Classroom.where(user_id: 4)
+    @s_classes = SClass.where(user_id: 4)
   end
 end
