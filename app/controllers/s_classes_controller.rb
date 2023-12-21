a13d15f03b4ceef585ef45237c89009c8d8cb571
Rina Iwamine <leddihome@gmail.com>
2023-12-21 18:56:37 +0900

    made a redirect route from class to classroom

--- app/controllers/s_classes_controller.rb
+++ app/controllers/s_classes_controller.rb
@@ -4,12 +4,18 @@ class SClassesController < ApplicationController
   def show
     @students = Student.where(s_class: @s_class)
     @student = Student.new
+    @classrooms = Classroom.where(user: current_user)
   end
 
   def create
   end
 
   def destroy
+  end
+
+  def redirect_to_classroom
+    classroom = Classroom.find(params[:classroom][:classroom_id])
+    redirect_to classroom_path(classroom, s_class_id: params[:classroom][:classroom_id])
   end
 
   private
