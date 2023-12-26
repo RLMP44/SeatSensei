70174f2e3435474a1ea4817f7b8fdc89576e95f2
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2023-12-26 17:27:04 +0900

    Made new button animation in the homepage

--- db/schema.rb
+++ db/schema.rb
@@ -101,12 +101,12 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_20_033109) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "username", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
