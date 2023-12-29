1cc008da959a54bc3749c8e7c6bccb9c3f5008c2
Rina Iwamine <leddihome@gmail.com>
2023-12-29 23:22:13 +0900

    tried fixing classroom create test

--- app/controllers/s_classes_controller.rb
+++ app/controllers/s_classes_controller.rb
@@ -8,9 +8,19 @@ class SClassesController < ApplicationController
   end
 
   def create
+    @s_class = SClass.new(s_class_params)
+    @s_class.user = current_user
+
+    if @s_class.save
+      redirect_to s_class_path(@s_class)
+    else
+      render "pages/home", status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @s_class.destroy
+    redirect_to root_path
   end
 
   def redirect_to_classroom
@@ -19,6 +29,10 @@ class SClassesController < ApplicationController
   end
 
   private
+
+  def s_class_params
+    params.require(:s_class).permit(:name, :year, :kumi, :subject, :schedule)
+  end
 
   def set_s_class
     @s_class = SClass.find(params[:id])
--- spec/system/classrooms/create_classroom_spec.rb
+++ spec/system/classrooms/create_classroom_spec.rb
@@ -5,19 +5,30 @@ RSpec.describe "CreateClassroom", type: :system do
     it "creates a classroom with correct parameters" do
       sign_in(create(:user))
       visit root_path
-      click_button "New Classroom"
 
-      expect(page).to have_content("Add a new classroom") # might be useless
       expect(Classroom.count).to eq(0)
+      expect(page).to have_content("No classrooms yet!")
 
-      fill_in 'Name', with: "New Classroom"
-      fill_in 'Total rows', with: 6
-      fill_in 'Total columns', with: 4
-      fill_in 'Max seats', with: 24
+      click_button "New Classroom"
+      within("#staticBackdropClassroom") do
+        fill_in 'Name', with: "New Classroom"
+        fill_in 'Total rows', with: 6
+        fill_in 'Total columns', with: 4
+        fill_in 'Max seats', with: 24
+        click_button "Save Classroom"
+      end
 
-      click_button "Save Classroom"
+      # visit root_path
 
-      expect(page).to have_content("Classroom was successfully created.")
+      # Capybara.using_wait_time 5 do
+      #   find("classroom-cards")
+      # end
+
+      find("classroom-cards")
+      within("classroom-cards") do
+        expect(page).to have_content("New Classroom")
+      end
+
       expect(Classroom.count).to eq(1)
       expect(Classroom.last.name).to eq("New Classroom")
     end
