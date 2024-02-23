ff200521d0a64c788976d02d015bc73f2e8928b7
Rina Iwamine <leddihome@gmail.com>
2024-02-23 19:58:08 +0900

    fixed student details

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ gem "simple_form", github: "heartcombo/simple_form"
 gem "sassc-rails"
 gem "cloudinary"
 gem 'factory_bot_rails'
+gem 'rack-cors'
 
 group :development, :test do
   gem "dotenv-rails"
--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -3,8 +3,6 @@ class ClassroomsController < ApplicationController
 
   def show
     @s_class = SClass.find(params[:s_class_id])
-    # arr = create_arr(@s_class)
-    # @hash = {s_class_id: @s_class.id, students: arr}.to_json
     if @classroom.arrangements.where(s_class: @s_class).present?
       @arrangement = @classroom.arrangements.find_by(s_class: @s_class)
     else
@@ -38,16 +36,6 @@ class ClassroomsController < ApplicationController
   end
 
   private
-
-  def create_arr(s_class)
-    s_class.students.map do |student|
-      {
-        student_id: student.id,
-        row: student.seat.row,
-        column: student.seat.column
-      }
-    end
-  end
 
   def classroom_params
     params.require(:classroom).permit(:name, :total_rows, :total_columns, :max_seats)
